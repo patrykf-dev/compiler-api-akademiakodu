b72ca0269470f0dad826766e228fa8bf8c539b77
Patryk <patryk.gsrmeen@gmail.com>
2019-05-05 18:40:40 +0200

    Refactor /upload request

--- src/main/java/pl/akademiakodu/controllers/CompilerApiController.java
+++ src/main/java/pl/akademiakodu/controllers/CompilerApiController.java
@@ -1,7 +1,9 @@
 package pl.akademiakodu.controllers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -13,16 +15,16 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.Executor;
 
 @CrossOrigin
 @RestController
 public class CompilerApiController {
 
     public static final String UPLOADED_FILES_STORAGE = "D:\\Development\\compiler-api-akademiakodu-results\\";
-    private CompilerApiService compilerApiService;
 
     @CrossOrigin
-    @PostMapping("/api/v1/code/upload")
+    @PostMapping("/code/upload")
     public ResponseEntity<String> singleFileUpload(@RequestParam("file") MultipartFile file) throws UnsupportedEncodingException {
 
         if (file.isEmpty()) {
@@ -39,14 +41,14 @@ public class CompilerApiController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        String result = "BLANK_RESULT";
-        synchronized (this) {
-            String sourceCode = new String(bytes, StandardCharsets.UTF_8);
-            compilerApiService = new CompilerApiService(sourceCode, "asd");
-            result = compilerApiService.getResult();
-        }
+        String sourceCode = new String(bytes, StandardCharsets.UTF_8);
+        CompilerApiService compilerApiService = new CompilerApiService(sourceCode, "Hello world");
+        String result = compilerApiService.getResult();
 
-        return new ResponseEntity<>("Successfully uploaded file " + result, HttpStatus.OK);
+
+        String info = "Successfully uploaded file! Result is:\n" +
+                result;
+        return new ResponseEntity<>(info, HttpStatus.OK);
     }
 
 }
