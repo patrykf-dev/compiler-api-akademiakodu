eed939455beb2a5915ec4c914f397c9ead4d30d7
Patryk <patryk.gsrmeen@gmail.com>
2019-05-17 22:59:16 +0200

    Update FileStorageService.java

--- src/main/java/pl/akademiakodu/services/FileStorageService.java
+++ src/main/java/pl/akademiakodu/services/FileStorageService.java
@@ -14,12 +14,18 @@ public class FileStorageService {
     public FileStorageService() {}
 
     public Path saveUploadedFile(byte[] fileContent, String originalFilename) throws IOException {
-        Path path = Paths.get(UPLOADED_FILES_STORAGE + originalFilename);
+        Path path = Paths.get(UPLOADED_FILES_STORAGE + "com\\example\\"+ originalFilename);
         Files.write(path, fileContent);
         return path;
     }
 
+    private static int counter = 0;
     public String getOutputPathForFile() {
-        return UPLOADED_FILES_STORAGE + "output\\result.txt";
+        counter++;
+        return UPLOADED_FILES_STORAGE + "output\\result" + Integer.toString(counter) + "-out.txt";
+    }
+
+    public String getErrorPathForFile() {
+        return UPLOADED_FILES_STORAGE + "output\\result" + Integer.toString(counter) + "-err.txt";
     }
 }
